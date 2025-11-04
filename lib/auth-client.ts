86f793215b0d2c74215d2efbbaba3edc6bdbb042
Robert <robert.popa@itsmartsystems.eu>
2025-11-04 10:02:40 +0200

    removed smth

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,7 +3,6 @@ import { expoClient} from "@better-auth/expo/client"
 import * as SecureStore from "expo-secure-store";
 
 export const authClient = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
     baseURL: "http://localhost:3000/",
         plugins: [
         expoClient({
